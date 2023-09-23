abc1d96cac76cc6f633daf47b2a0b1f37e56354d
Marcos Franco <v.marcos.franco@gmail.com>
2023-09-23 17:31:14 +0100

    Asserts

--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -1,6 +1,14 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('Localizando por data-test-id', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/v1/');
   await page.getByTestId('username').fill('Marcos');
 });
+
+test('Asserts básicos', async({ page }) => {
+  await page.goto('https://www.saucedemo.com/v1/');
+  const button = page.locator('input[id="login-button"]');
+  await expect(button).toHaveCSS('background-color', 'rgb(226, 35, 26)');
+  await expect(button).toHaveAttribute('value','LOGIN');
+  await expect(button).toBeVisible();
+})
