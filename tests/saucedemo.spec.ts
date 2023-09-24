050c4576a52dca4a8ecff7b07b9362a10ff6fa45
Marcos Franco <v.marcos.franco@gmail.com>
2023-09-24 20:00:52 +0100

    aula 11 - implementacoes

--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -5,10 +5,12 @@ test('Localizando por data-test-id', async ({ page }) => {
   await page.getByTestId('username').fill('Marcos');
 });
 
-test('Asserts básicos', async({ page }) => {
+test.only('Asserts básicos', async({ page }) => {
   await page.goto('https://www.saucedemo.com/v1/');
-  const button = page.locator('input[id="login-button"]');
-  await expect(button).toHaveCSS('background-color', 'rgb(226, 35, 26)');
-  await expect(button).toHaveAttribute('value','LOGIN');
-  await expect(button).toBeVisible();
+  const loginButton = await page.locator('input#login-button');
+  await expect.soft(loginButton).toHaveCSS('background-color','rgb(225, 35, 26)');
+  await expect(loginButton).not.toHaveCSS('background-color','rgb(0, 0, 0)');
+  await expect(loginButton).toHaveAttribute('value','LOGIN');
+  await expect.soft(loginButton, 'Botão está visível').not.toBeVisible();
+  await expect(loginButton).not.toHaveCSS('background-color','rgb(0, 0, 0)');
 })
