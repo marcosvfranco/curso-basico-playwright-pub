f7cc25c21a0e0a78674161389b21ce4c76849cf4
Marcos Franco <v.marcos.franco@gmail.com>
2023-12-01 18:43:37 +0000

    aula 15 - desafio

---
+++ tests/desafio.spec.ts
@@ -0,0 +1,21 @@
+// https://the-internet.herokuapp.com/
+import { expect, test } from '@playwright/test';
+// https://www.saucedemo.com/v1/
+
+// 1- Login com sucesso
+// Usar usuário standard_user
+// - Verificar URL Pagina 
+// - Verificar pelo menos 1 item da pagina final (visible)
+test('Desafio 1', async({ page }) => {
+})
+
+// 2- Login com usuario locked
+// Usar usuário locked_out_user
+// - Verificar Mensagem de erro
+test('Desafio 2', async({ page }) => {
+})
+
+// 3- Login senha errada
+// - Verificar Mensagem de erro
+test('Desafio 3', async({ page }) => {
+})
