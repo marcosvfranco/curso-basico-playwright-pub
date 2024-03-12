c12ef0858eb3d49c1a24a57c4eb6ffcc30e316b8
Marcos Franco <v.marcos.franco@gmail.com>
2024-03-12 19:34:39 +0000

    aula before hooks

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,12 +2,24 @@
 import { expect, test } from '@playwright/test';
 // https://www.saucedemo.com/v1/
 
-// test suites
-// Logins de sucesso
-// Falhas de Login
+/**
+ * Test Hooks
+ * test.beforeEach - antes de cada
+ * test.beforeAll - antes de todos
+ * test.afterEach - depois de cada
+ * test.afterAll - depois de todos
+ */
+
 test.describe('Logins de sucesso', async () => {
-    test('Usuário e senha corretos - usuário comum', async({ page }) => {
+    test.beforeEach(async ({page}) => {
         await page.goto('https://www.saucedemo.com/v1/');
+    })
+    test.beforeAll(async () => {
+        console.log('Começando testes de login de sucesso');
+    })
+
+    test('Usuário e senha corretos - usuário comum', async({ page }) => {
+        // await page.goto('https://www.saucedemo.com/v1/');
         await page.locator('[data-test="username"]').fill('standard_user');
         await page.locator('[data-test="password"]').fill('secret_sauce');
         await page.locator('input#login-button').click();
@@ -19,10 +31,17 @@ test.describe('Logins de sucesso', async () => {
 })
 
 test.describe('Falhas no Login', async () => {
+    test.beforeEach(async ({page}) => {
+        await page.goto('https://www.saucedemo.com/v1/');
+    })
+
+    test.beforeAll(async () => {
+        console.log('Começando testes de falha de login');
+    })
     test('Login com usuario locked', async({ page }) => {
         const errorLabel = page.locator('[data-test="error"]');
 
-        await page.goto('https://www.saucedemo.com/v1/');
+        // await page.goto('https://www.saucedemo.com/v1/');
         await expect(errorLabel).toBeHidden();
 
         await page.locator('[data-test="username"]').fill('locked_out_user');
@@ -34,7 +53,7 @@ test.describe('Falhas no Login', async () => {
     })
 
     test('Login senha errada', async({ page }) => {
-        await page.goto('https://www.saucedemo.com/v1/');
+        // await page.goto('https://www.saucedemo.com/v1/');
         await page.locator('[data-test="username"]').fill('standard_user');
         await page.locator('[data-test="password"]').fill('asdf');
         await page.locator('input#login-button').click();
